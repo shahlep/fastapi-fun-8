894a02c2148d5d8ca87d924839f5bcfa8d146012
Shah rahman <toshah.rahman@gmail.com>
2022-06-03 17:54:55 +0300

    fastapi fun 13

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from backend.core.config import settings
+from backend.db.session import engine
+from backend.db.base_class import Base
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title=settings.PROJECT_TITLE,
