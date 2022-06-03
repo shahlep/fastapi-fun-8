62c19e29af3735c0106ede191cd29181f954f511
Shah rahman <toshah.rahman@gmail.com>
2022-06-03 16:53:35 +0300

    fastapi fun 7

--- core/config.py
+++ core/config.py
@@ -10,5 +10,11 @@ class Settings:
     PROJECT_TITLE: str = "Jobboard"
     PROJECT_VERSION: str = "0.1.1"
 
+    POSTGRESS_USER :str = os.getenv('POSTGRESS_USER')
+    POSTGRESS_PASSWORD:str = os.getenv('POSTGRESS_PASSWORD')
+    POSTGRESS_SERVER :str= os.getenv('POSTGRESS_SERVER')
+    POSTGRESS_PORT:str = os.getenv('POSTGRESS_PORT')
+    POSTGRESS_DB :str = os.getenv('POSTGRESS_DB')
+    DATABASE_URL= f''
 
 settings = Settings()
