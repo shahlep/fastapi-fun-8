c86d7ebe5f650feff370479c6880814ee344c873
Shah rahman <toshah.rahman@gmail.com>
2022-06-03 17:38:28 +0300

    fastapi fun 11

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,3 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from backend.core.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+
