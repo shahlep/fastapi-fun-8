30ef5116df50f45b41f45a2cae1d8d06404a38d4
Shah rahman <toshah.rahman@gmail.com>
2022-06-03 17:56:48 +0300

    fastapi fun 14

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
-app = FastAPI(
-    title=settings.PROJECT_TITLE,
-    version=settings.PROJECT_VERSION)
+def start_application():
+    app = FastAPI(
+        title=settings.PROJECT_TITLE,
+        version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
