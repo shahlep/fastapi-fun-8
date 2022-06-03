5b125015eca29b1be462e30d1e030a00875f4b36
Shah rahman <toshah.rahman@gmail.com>
2022-06-03 16:59:31 +0300

    fastapi fun 9

--- backend/core/config.py
+++ backend/core/config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-env_path = Path('.') / '.env'
+env_path = Path('') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from core.config import settings
+from backend.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_TITLE,
