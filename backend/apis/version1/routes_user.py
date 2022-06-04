9eb7bb1681a91e93a4c891fd80a4aec66a1da445
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 12:30:50 +0300

    fastapi fun 40

--- backend/apis/version1/routes_user.py
+++ backend/apis/version1/routes_user.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from backend.schemas.users import UserCreate
+from backend.schemas.users import UserCreate, ShowUser
 from backend.db.session import get_db
 from backend.db.repository.users import create_new_user
 
 router = APIRouter()
 
 
-@router.post("/")
+@router.post("/", response_model=ShowUser)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user, db)
     return user
