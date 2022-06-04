accbc2534d564e648bb1f188cf9b3dedb5d1c625
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 17:22:31 +0300

    fastapi fun 64

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from backend.apis.version1 import routes_user
+from backend.apis.version1 import routes_user,routes_job
 
 api_router = APIRouter()
 
 api_router.include_router(routes_user.router, prefix="/user", tags=["users"])
+api_router.include_router(routes_job.router,prefix="/job",tags=["jobs"])
