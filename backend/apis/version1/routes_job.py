fbadb6c0edf091531e9deb083720d0f357b04b4b
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 17:07:07 +0300

    fastapi fun 62

--- backend/apis/version1/routes_job.py
+++ backend/apis/version1/routes_job.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from backend.schemas.jobs import JobCreate,ShowJobs
+from backend.schemas.jobs import JobCreate, ShowJobs
 from backend.db.session import get_db
 from backend.db.repository.jobs import create_new_job
 
+router = APIRouter()
+
+
+@router.post('/jobs', response_model=ShowJobs)
+def create_job(job: JobCreate, db: Session = Depends(get_db)):
+    job = create_new_job(job, db)
+    return job
