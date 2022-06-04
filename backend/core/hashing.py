ae0a651a59f33d18dfa222eef0990e6b3e92b282
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 10:10:13 +0300

    fastapi fun 28

--- backend/core/hashing.py
+++ backend/core/hashing.py
@@ -7,3 +7,7 @@ class Hash():
     @staticmethod
     def get_hash_password(plain_password):
         return pwd_context.hash(plain_password)
+
+    @staticmethod
+    def verify_password(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
