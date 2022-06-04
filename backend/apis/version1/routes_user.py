820b290589efd663fa4ac61c017aad5da6fd1075
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 10:46:29 +0300

    fastapi fun 33

--- backend/apis/version1/routes_user.py
+++ backend/apis/version1/routes_user.py
@@ -6,3 +6,9 @@ from backend.db.session import get_db
 from backend.db.repository.users import create_new_user
 
 router = APIRouter()
+
+
+@router.post('/users')
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    user = create_new_user(user, db)
+    return user
