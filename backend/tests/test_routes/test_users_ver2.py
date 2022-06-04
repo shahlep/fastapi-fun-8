f825a699111e2a3b78964468f63e641290a1b9fb
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 15:45:36 +0300

    fastapi fun 53

--- backend/tests/test_routes/test_users_ver2.py
+++ backend/tests/test_routes/test_users_ver2.py
@@ -1,7 +1,12 @@
+from fastapi.testclient import TestClient
 import json
 
+from main import app
 
-def create_user(client):
+client = TestClient(app)
+
+
+def test_create_user():
     data = {"username": "test", "email": "test@example.com", "password": "123456"}
     response = client.post("/user", json.dump(data))
     assert response.status_code == 200
