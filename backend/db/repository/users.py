e6538c6ce69cc4dcdadf6f21c82f231623888a3c
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 10:36:06 +0300

    fastapi fun 31

--- backend/db/repository/users.py
+++ backend/db/repository/users.py
@@ -2,3 +2,16 @@ from sqlalchemy.orm import Session
 from backend.schemas.users import UserCreate
 from backend.db.models.users import User
 from backend.core.hashing import Hash
+
+
+def create_new_user(user:UserCreate,db:Session):
+    user = User(username=user.username,
+                email=user.email,
+                hashed_password=Hash.get_hash_password(user.password),
+                is_active=True,
+                is_superuser=False
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
