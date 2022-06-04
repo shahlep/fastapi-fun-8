af885eff101f3897825594d20f4b603952d5503b
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 10:09:03 +0300

    fastapi fun 27

--- backend/core/hashing.py
+++ backend/core/hashing.py
@@ -1,3 +1,9 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class Hash():
+    @staticmethod
+    def get_hash_password(plain_password):
+        return pwd_context.hash(plain_password)
