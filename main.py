30122514c78e31ef5fafa5485c0729d835f46a31
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 12:16:16 +0300

    fastapi fun 37

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from fastapi import FastAPI
 from backend.core.config import settings
 from backend.db.session import engine
 from backend.db.base import Base
-from backend.apis.version1.routes_user import router
+from backend.apis.base import api_router
 
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
+def include_router(app):
+    app.include_router(api_router)
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
     create_tables()
-    app.include_router(router)
+    include_router(app)
     return app
 
 
