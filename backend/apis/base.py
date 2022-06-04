d5c90087ab5f76f107f989213d9852fd0de83a28
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 12:10:55 +0300

    fastapi fun 36

---
+++ backend/apis/base.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from backend.apis.version1 import routes_user
+
+
+api_router = APIRouter
+
+api_router.include_router(routes_user.router,prefix='/user',tags=['users'])
+
