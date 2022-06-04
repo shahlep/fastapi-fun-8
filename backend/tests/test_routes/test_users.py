ad0deeba547a2b1724f2df4d98ccfba7504f3a89
Shah rahman <toshah.rahman@gmail.com>
2022-06-04 15:01:40 +0300

    fastapi fun 52

--- backend/tests/test_routes/test_users.py
+++ backend/tests/test_routes/test_users.py
@@ -5,3 +5,5 @@ def test_create_user(client):
     data = {"username": "test", "email": "test@example.com", "password": "123456"}
     response = client.post("/user", json.dump(data))
     assert response.status_code == 200
+    assert response.json()["email"] == "test@example.com"
+    assert response.json()["is_active"] == True
