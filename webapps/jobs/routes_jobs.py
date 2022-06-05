6485e8f43263ea04d2157dda968d804900fdcdfe
Shah rahman <toshah.rahman@gmail.com>
2022-06-05 13:33:06 +0300

    fastapi fun 75

--- webapps/jobs/routes_jobs.py
+++ webapps/jobs/routes_jobs.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi import requests
+from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
