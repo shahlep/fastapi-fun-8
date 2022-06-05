0ce05260926004cd7ffb8626fc86e46efccd9dec
Shah rahman <toshah.rahman@gmail.com>
2022-06-05 14:52:02 +0300

    fastapi fun 88

--- main.py
+++ main.py
@@ -26,15 +26,5 @@ def start_application():
 app = start_application()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
 if __name__ == "__main__":
     uvicorn.run(app)
