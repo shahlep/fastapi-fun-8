68681368c548fa0c0ee54a95d3846cb8e6f74742
Shah rahman <toshah.rahman@gmail.com>
2022-06-05 13:30:14 +0300

    fastapi fun 74

--- webapps/jobs/routes_jobs.py
+++ webapps/jobs/routes_jobs.py
@@ -2,3 +2,7 @@ from fastapi import APIRouter
 from fastapi import requests
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+
+templates = Jinja2Templates(directory='templates')
+router = APIRouter()
